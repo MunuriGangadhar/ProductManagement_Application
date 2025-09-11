b46767eb3f7372946691f91a443b87b7868be150
Munuri-Gangadhar <munurigangadhar0987@gmail.com>
2025-09-11 11:08:14 +0530

    few changes are made

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,7 +3,15 @@ import cors from 'cors';
 import productRoutes from "./routes/productRoutes";
 
 const app=express();
-app.use(cors());
+// app.use(cors());
+app.use(cors({
+  origin: [
+    'http://localhost:3000', 
+    'https://product-management-application-sigma.vercel.app/'
+  ],
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type']
+}));
 app.use(express.json());
 app.use('/api',productRoutes);
 export default app;
